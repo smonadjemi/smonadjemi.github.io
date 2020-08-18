0770204f02f5916451d2182ecefe5e6a768c7cd2
Shayan Monadjemi <smonadjemi@shayans-macbook-pro.local.dhcp.wustl.edu>
2020-08-18 15:30:09 -0500

    push first build

---
+++ build/precache-manifest.d9c0735d3f2eaa1e570a62bd6fdd1b58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac54966a694ed2c738f4d255b6a85ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f856d543f84036ee7b25",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dc089856a44e4c2922da",
+    "url": "/static/css/main.170d9f0a.chunk.css"
+  },
+  {
+    "revision": "f856d543f84036ee7b25",
+    "url": "/static/js/2.590b9908.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.590b9908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc089856a44e4c2922da",
+    "url": "/static/js/main.c242ba80.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  },
+  {
+    "revision": "3ea513a6b87ab77e9c67e8223e7ca116",
+    "url": "/static/media/img.3ea513a6.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
