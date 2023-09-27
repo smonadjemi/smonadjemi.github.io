b0e2c18c4520c94ac0a8f6fe3cb765e49bf24f03
Shayan Monadjemi <smonadjemi@Shayans-MacBook.attlocal.net>
2023-09-26 22:37:52 -0400

    updated source

--- src/components/single_publication.js
+++ src/components/single_publication.js
@@ -8,13 +8,40 @@ import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
 
 
 function Publication(props){
+    var authors_list = props.publication.authors.split(', ')
+    var authors_list_string = authors_list.map((x, i) => {
+        if (x == 'Shayan Monadjemi')
+        {
+            if (i == authors_list.length - 1)
+                return <><b>{x}</b></>
+            else
+                return <><b>{x}</b>, </>
+        }
+        else
+        {
+            if (i == authors_list.length - 1)
+                return <>{x}</>
+            else
+                return <>{x}, </>
+        }
+        
+    })
+
+
+    
     return(
 <>
     <Row className='single-publication'>
         <Col lg={12}>
             <div>{props.publication.title}</div>
-            <div className='text-muted'>{props.publication.authors}</div>
-            <div className='text-muted'><em>{props.publication.venue}</em></div>
+            <div className='text-muted'>{authors_list_string}</div>
+            <div className='text-muted'><em>{props.publication.venue}</em>. {props.publication.year}.</div>
+            <div>
+                <span class="badge badge-secondary">{props.publication.paper_tag}</span>
+                {
+                    Object.keys(props.publication.links).map((x)=> {return <a className='pub-link' href={props.publication.links[x]} target='_blank'>{x}</a>})
+                }
+            </div>
         </Col>
     </Row>
 </>
